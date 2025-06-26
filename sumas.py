16ff2c64105c852632ca6cbab3e6e3bee2c8d889
sergio pizarro berrios <ser.pizarrob@duocuc.cl>
2025-06-26 10:24:15 -0400

    atualizador
    archivo atualizado

--- sumas.py
+++ sumas.py
@@ -1,5 +1,10 @@
 def sumar(a,b):
     return a + b
+
+def restar(a,b):
+    return a-b
+
 n1= int(input("ingrese un numero : "))
 n2= int(input("ingrese un numero : "))
-print(sumar(n1+n2))
+print("sumas",sumar(n1+n2))
+print("restas",restar(n1-n2))
